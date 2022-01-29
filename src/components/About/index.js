9e0ddf2db5aa48004b0fb708486a9a05ab294066
Sophia Barrett <sophia@yoursummit.media>
2022-01-29 09:07:30 -0700

    add about section and initial styles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,53 @@
-function About() {  
-    return (
-      <div>
-        <h2>Hi, I'm Sophie.</h2>
-      </div>
-    );
-  }
-  
-  export default About;
-  
+function About() {
+  return (
+    <div>
+      <section id="intro">
+        <img
+          src={require("../../assets/images/sophia-barrett.png")}
+          alt="Sophia Barrett"
+        />
+        <p>
+          Hi, I'm Sophia.
+          <br />I design and build on-brand web applications and user
+          experiences.
+        </p>
+      </section>
+      <section id="about">
+        <div className="title-wrapper">
+          <h2>About</h2>
+          <img
+            src={require("../../assets/images/about-me.png")}
+            alt="Sophia Barrett and partner on a hike."
+          />
+        </div>
+        <p>
+          <span class="paragraph-start">I have always been an artist</span> with
+          a seemingly inharmonious love for order and logic. Growing up and
+          discovering myself inside of that dichotomy was (and is) confusing. In
+          school, math was interesting, accessible and gratifying. But art was
+          alluring and challenging and oh so soul nourishing. Neither felt fully
+          like me. So, I went off to college without a clue and without pursuing
+          either. Then, in my first year, fate sent me a friend in the
+          Department of Design and I discovered my passion at the intersection
+          of form and function.
+        </p>
+        <p>
+          <span class="paragraph-start">Almost a decade later,</span> I am still
+          exploring that intersection in my work. I’ve worked across countless
+          industries helping small business owners, department heads, startups
+          and artists create beautiful solutions to their practical needs. From
+          creating recognizable logos, to designing seamless customer journeys,
+          to building easy-to-use websites and applications, each of my projects
+          delivers a compelling on-brand experience layered over powerful
+          back-end performance.
+        </p>
+        <p>
+          I am currently living in Gilbert, Arizona with my partner, our two
+          orange tabbies and several cacti.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default About;
